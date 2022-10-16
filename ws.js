2a8ca8e59fd8eec758751ce73652c593b4bc662b
Silver Luhtoja <silver.luhtoja@gmail.com>
2022-10-16 16:52:22 +0300

    fix

--- ws.js
+++ ws.js
@@ -14,17 +14,6 @@ Array.prototype.remove = function () {
     return this;
 };
 
-Array.prototype.remove = function () {
-    let what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
-
 const matchPlayerIPWithRoomId = {}
 const playerMoving = [];
 

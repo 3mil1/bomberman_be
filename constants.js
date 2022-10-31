5e92b50372d25d9c7bcc048d1efc268f442b6255
Valeria Kharchenko <valerie.kharchenko@gmail.com>
2022-10-31 19:37:13 +0300

    change constants to correct

--- constants.js
+++ constants.js
@@ -4,7 +4,6 @@ export const START_GAME = 'startGame';
 export const SET_BOMB = 'setBomb';
 export const CLOSE_CONNECTION = 'closeConnection';
 export const GET_ROOMS = 'getRooms';
-// export const DECREASE_HEALTH = 'decreaseHealth';
 export const SET_POSITION = 'setPosition';
 export const NEW_MESSAGE = 'newMessage'
 export const DELETE_ROOM = 'deleteRoom'
@@ -16,6 +15,6 @@ export const LOOSER = 'looser';
 export const TIE = 'tie';
 
 //timer
-export const WAITING_TIMER = 5;//for test
-export const COUNTDOWN_TIMER = 3;//for test
-export const GAME_OVER_TIMER = 30;//for test
+export const WAITING_TIMER = 20;
+export const COUNTDOWN_TIMER = 10;
+export const GAME_OVER_TIMER = 30;
